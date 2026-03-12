e914c579501d783f992ff064202eece48f46216e
Gregory <joystickgrindhouse@gmail.com>
2026-03-11 20:32:37 -0700

    Create main.js

---
+++ public/golf/main.js
@@ -0,0 +1,22 @@
+
+const root = document.getElementById("golf-root")
+
+root.innerHTML = `
+
+<div style="
+color:white;
+background:black;
+height:100vh;
+display:flex;
+align-items:center;
+justify-content:center;
+flex-direction:column;
+font-family:sans-serif;
+">
+
+<h1>Rivalis Golf</h1>
+
+<p>Golf module loaded successfully.</p>
+
+</div>
+
