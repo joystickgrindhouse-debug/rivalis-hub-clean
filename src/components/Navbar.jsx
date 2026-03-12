ab140d2b83253a3cf3db1a106cfb6246cf4a6102
Gregory <joystickgrindhouse@gmail.com>
2026-03-11 20:14:38 -0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+```jsx
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { auth } from "../firebase.js";
@@ -166,7 +167,7 @@ export default function Navbar({ user, userProfile, theme, cycleTheme }) {
                 Leaderboard
               </Link>
 
-              {userProfile?.role === "admin" && (
+              {userProfile?.isAdmin && (
                 <Link
                   to="/admin-control"
                   onClick={closeDropdown}
@@ -199,3 +200,4 @@ export default function Navbar({ user, userProfile, theme, cycleTheme }) {
   );
 
 }
+```
